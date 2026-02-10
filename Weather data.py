fa3139c6eb84318a10f73df1559fca8793e7bf13
elsig <as50mg@student.aau.dk>
2026-02-10 09:54:24 +0100

    calculated wind speed 2013

--- Weather data.py
+++ Weather data.py
@@ -1,1 +1,10 @@
 import numpy as np
+import pandas as pd
+
+t=np.arange(1, 4079, 1)
+
+df = pd.read_excel("vejrdata/Samlet vejrdata 2013.2019-NK.xlsx",sheet_name='2013')
+windspeed2013 = df.iloc[1:, 2].values
+print(windspeed2013)
+
+
