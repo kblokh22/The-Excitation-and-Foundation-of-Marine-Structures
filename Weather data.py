a02fb11ef64058802e608ae4cd8c54826fe5f573
elsig <as50mg@student.aau.dk>
2026-02-10 10:50:48 +0100

    introduced temp

--- Weather data.py
+++ Weather data.py
@@ -12,6 +12,16 @@ time=df.iloc[2:, 0].values
 print(time)
 
 z=15 #height of the measurement equipment of the wind
+U_z=windspeed
+U_10=U_z*(10/z)**(1/7)
 
+print(U_10)
+
+airtemp=df.iloc[4:, 1].values
+airtemp=airtemp.astype(float)
+seatemp=df.iloc[11:, 2].values
+seatemp=seatemp.astype(float)
+DeltaT=airtemp-seatemp
+print(DeltaT)
 
 
