4afaf5d6219eb925d482335ba6a3bc84de14973c
elsig <as50mg@student.aau.dk>
2026-02-10 10:35:55 +0100

    calculated wind speed 2018 to 25

--- Weather data.py
+++ Weather data.py
@@ -1,10 +1,17 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-t=np.arange(1, 4079, 1)
 
-df = pd.read_excel("vejrdata/Samlet vejrdata 2013.2019-NK.xlsx",sheet_name='2013')
-windspeed2013 = df.iloc[1:, 2].values
-print(windspeed2013)
+df = pd.read_excel("vejrdata/Vejrdata 2018-2025.xlsx",sheet_name='Vejrdata 2018-2025')
+windspeed = df.iloc[2:, 2].values
+windspeed=windspeed.astype(float)
+print(windspeed)
+
+time=df.iloc[2:, 0].values
+print(time)
+
+z=15 #height of the measurement equipment of the wind
+
 
 
