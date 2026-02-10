324b7b642dcd57c1f1c60acca6786bca754e05a9
elsig <as50mg@student.aau.dk>
2026-02-10 11:25:22 +0100

    started working on RT

--- Weather data.py
+++ Weather data.py
@@ -17,11 +17,39 @@ U_10=U_z*(10/z)**(1/7)
 
 print(U_10)
 
-airtemp=df.iloc[4:, 1].values
+airtemp=df.iloc[2:, 5].values
 airtemp=airtemp.astype(float)
-seatemp=df.iloc[11:, 2].values
+seatemp=df.iloc[2:, 12].values
 seatemp=seatemp.astype(float)
 DeltaT=airtemp-seatemp
-print(DeltaT)
+DeltaT[DeltaT<-50]=np.nan #remove broken values
+print(np.nanmin(DeltaT))
+print(np.nanmax(DeltaT))
+
+#assign values of R_T
+R_T = []
+for dt in DeltaT:
+    if -15 <= dt < -12.5:
+        R_T.append(1.21)
+    elif -12.5 <= dt < -10:
+        R_T.append(1.19)
+    elif -10 <= dt < -7.5:
+        R_T.append(1.19)
+    elif -7.5 <= dt < -5:
+        R_T.append(1.16)
+    elif -5 <= dt < -2.5:
+        R_T.append(1.13)
+    elif -2.5 <= dt < -0:
+        R_T.append(1)
+    elif 0 <= dt < 2.5:
+        R_T.append(0.92)
+    elif 2.5 <= dt < 5:
+        R_T.append(0.88)
+    elif 5 <= dt < 7.5:
+        R_T.append(0.84)
+    elif 7.5 <= dt < 10:
+        R_T.append(0.83)
+    else:
+        R_T.append(None)  # hvis værdien ikke matcher et interval
 
 
