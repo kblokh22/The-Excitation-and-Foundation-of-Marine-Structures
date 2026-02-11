6dc7234ad2469aafbaec53c30171d6e4c32f30ba
Kasper Blokhus <kasperblokhus@gmail.com>
2026-02-11 09:07:08 +0100

    Standardize file paths: rename `vejrdata` to `Weather data`, update file references in related scripts.

--- Weather data.py
+++ Weather data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_excel("vejrdata/Vejrdata 2018-2025.xlsx",sheet_name='Vejrdata 2018-2025')
+df = pd.read_excel("Weather data/Vejrdata 2018-2025.xlsx",sheet_name='Vejrdata 2018-2025')
 windspeed = df.iloc[2:, 2].values
 windspeed=windspeed.astype(float)
 print(windspeed)
