eabbeb447e3fed4505258be1211cd5120bc87ec3
Kasper Blokhus <kasperblokhus@gmail.com>
2026-02-11 14:27:34 +0100

    Load extended weather dataset, parse key columns, and enhance water level visualization.

--- wave data analysis.py
+++ wave data analysis.py
@@ -20,13 +20,21 @@ for year in years:
     mid_waves.extend(mid_wave.iloc[1:])
     max_waves.extend(max_wave.iloc[1:])
 
-print(f'Number of dates: {len(dates)}')
-print(f'Number of water levels: {len(water_levels)}')
-print(f'Number of mid waves: {len(mid_waves)}')
-print(f'Number of max waves: {len(max_waves)}')
+df = pd.read_excel("Weather data/Vejrdata 2018-2025.xlsx")
 
-print(type(dates[0]))
+date = pd.to_datetime(df['Column1'].iloc[2:].values, format='%d/%m/%Y %H:%M:%S')
+water_level = df['Column2'].iloc[2:].astype(float)
+mid_wave = df['Column7'].iloc[2:].astype(float)
+max_wave = df['Column8'].iloc[2:].astype(float)
+
+dates.extend(date)
+water_levels.extend(water_level)
+mid_waves.extend(mid_wave)
+max_waves.extend(max_wave)
 
 plt.figure()
 plt.plot(dates, water_levels)
+plt.xlabel('Date')
+plt.ylabel('Water level [m]')
+plt.title('Water level over time')
 plt.show()
