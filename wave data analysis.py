b2f949219b073cfa9e040b64923b10fb09aac667
Kasper Blokhus <kasperblokhus@gmail.com>
2026-02-11 11:27:31 +0100

    Initialize wave data analysis: load multi-year dataset, extract key metrics, and visualize water levels.

--- wave data analysis.py
+++ wave data analysis.py
@@ -1,4 +1,32 @@
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
+years = np.arange(2013,2019)
 
-df = pd.read_excel("Weather data/Vejrdata 2018-2025.xlsx",sheet_name='Vejrdata 2018-2025')
+dates = []
+water_levels = []
+mid_waves = []
+max_waves = []
+
+for year in years:
+    df = pd.read_excel("Weather data/Samlet vejrdata 2013.2019-NK.xlsx",sheet_name=f'{year}')
+    date = df['Tidspunkt']
+    water_level = df['Vandstand [m]']
+    mid_wave = df['Mid. bølg [m]']
+    max_wave = df['Max. bølg. [m]']
+    dates.extend(date.iloc[1:])
+    water_levels.extend(water_level.iloc[1:])
+    mid_waves.extend(mid_wave.iloc[1:])
+    max_waves.extend(max_wave.iloc[1:])
+
+print(f'Number of dates: {len(dates)}')
+print(f'Number of water levels: {len(water_levels)}')
+print(f'Number of mid waves: {len(mid_waves)}')
+print(f'Number of max waves: {len(max_waves)}')
+
+print(type(dates[0]))
+
+plt.figure()
+plt.plot(dates, water_levels)
+plt.show()
