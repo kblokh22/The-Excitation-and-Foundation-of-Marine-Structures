908247235fa458e6ee7e7ed15d2ddd98c8d29899
ludvi <ludvigskj392@gmail.com>
2026-02-13 09:56:16 +0100

    cpt3 gg

---
+++ CPT/Friction_angel_stat.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import io
+
+from matplotlib.pyplot import xlabel, ylabel, legend
+
+from CPT.CPT_Angle_of_friction import phi_efective_3
+
+#Starting by categorizing the data into different soils:
+
+phi_efective_3
