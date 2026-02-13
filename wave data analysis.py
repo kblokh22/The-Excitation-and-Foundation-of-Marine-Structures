15b6df23d3dc87a51571e7f5f5fff1ff6b2e68ff
Kasper Blokhus <kasperblokhus@gmail.com>
2026-02-13 14:16:39 +0100

    Refactor wave data extraction and enhance visualization with extreme wave markers.

--- wave data analysis.py
+++ wave data analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.ndimage import label
 
 years = np.arange(2013,2019)
 
@@ -11,14 +12,11 @@ max_waves = []
 
 for year in years:
     df = pd.read_excel("Weather data/Samlet vejrdata 2013.2019-NK.xlsx",sheet_name=f'{year}')
-    date = df['Tidspunkt']
-    water_level = df['Vandstand [m]']
-    mid_wave = df['Mid. bølg [m]']
-    max_wave = df['Max. bølg. [m]']
-    dates.extend(date.iloc[1:])
-    water_levels.extend(water_level.iloc[1:])
-    mid_waves.extend(mid_wave.iloc[1:])
-    max_waves.extend(max_wave.iloc[1:])
+
+    dates.extend(df['Tidspunkt'].iloc[1:])
+    water_levels.extend(df['Vandstand [m]'].iloc[1:])
+    mid_waves.extend(df['Mid. bølg [m]'].iloc[1:])
+    max_waves.extend(df['Max. bølg. [m]'].iloc[1:])
 
 df = pd.read_excel("Weather data/Vejrdata 2018-2025.xlsx")
 
@@ -32,9 +30,21 @@ water_levels.extend(water_level)
 mid_waves.extend(mid_wave)
 max_waves.extend(max_wave)
 
+dates = np.array(dates)
+water_levels = np.array(water_levels)
+mid_waves = np.array(mid_waves)
+max_waves = np.array(max_waves)
+max_waves[max_waves > 9] = np.nan
+
+indices_extreme = np.where(max_waves > 6)[0]
+
+H_extreme = max_waves[indices_extreme]
+dates_extreme = dates[indices_extreme]
+
 plt.figure()
-plt.plot(dates, water_levels)
+plt.plot(dates, max_waves)
+plt.plot(dates_extreme, H_extreme, 'ro')
 plt.xlabel('Date')
-plt.ylabel('Water level [m]')
+plt.ylabel('Significant wave height [m]')
 plt.title('Water level over time')
 plt.show()
