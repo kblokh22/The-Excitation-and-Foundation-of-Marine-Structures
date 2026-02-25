f0cca77cf45f05a0a7f9e66b0118bea1bafd9e0a
ludvi <ludvigskj392@gmail.com>
2026-02-25 11:29:19 +0100

    Bitch mikkel snitch mikkel fuck mikkel

--- Geo/Triaxial/CID & CIU.py
+++ Geo/Triaxial/CID & CIU.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 B = ["B8 805a ","B14 1423a", "B8 881a ", "B4 430a ", "B8 829a "]
 depth = ["22,0 - 22,5 m", "20,0 - 20,5 m", "25,0 - 25,6 m",  "32,0 - 32,6 m", "34,0 - 34,6 m"]
+Test = ["CID","CIU","CID","CID","CID"]
 
 q_tp = np.array([766, 1141, 489, 881, 793 ])
 sigma_3tp = np.array([848, 908, 1300, 1300, 1540 ])
@@ -35,10 +36,6 @@ tau_cs = q_cs/2
 
 
 
-
-
-
-
 df = pd.DataFrame()
 df['Boreholes'] = B
 df ['Depth' ] = depth
@@ -46,6 +43,7 @@ df['phi_tp'] = phi_tp
 df['phi_p'] = phi_p
 df['phi_cs'] = phi_cs
 df['alpha_p'] = alpha_p
+df['Type'] = Test
 
 
 print("-"*100)
