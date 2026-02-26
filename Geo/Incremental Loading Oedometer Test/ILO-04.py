af9c5afa7bcdca0d15842ac80b3a7208d6c00e01
ludvi <ludvigskj392@gmail.com>
2026-02-26 15:05:36 +0100

    ILO ÆNDRIGN

--- Geo/Incremental Loading Oedometer Test/ILO-04.py
+++ Geo/Incremental Loading Oedometer Test/ILO-04.py
@@ -2,5 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+B = np.array(['B04', 'B11'])
+depth = np.array(['29.53-29.56', '29.53-29.56'])
 
+Q = np.array([0.12,0.26])
+
+sigma_oc_akai = np.array([0,700])
+
+df = pd.DataFrame()
+df['Borehole'] = B
+df['Depth [m]'] = depth
+df['$Q_(1600-3200)$'] = Q
+
+print(df)
 
