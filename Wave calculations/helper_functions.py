a59c5584bef946bcee7e4a716eb5121f8dd318ed
Kasper Blokhus <kasperblokhus@gmail.com>
2026-03-10 11:11:46 +0100

    Add waveLengthIteration function for iterative wavelength calculation with convergence check and logging

--- Wave calculations/helper_functions.py
+++ Wave calculations/helper_functions.py
@@ -176,3 +176,33 @@ def calculate_gumbel_parameters(data):
     avg_relative_error = np.mean(relative_errors)
 
     return A_fit, B_fit, avg_relative_error
+
+def waveLengthIteration(wave_period, water_depth):
+    """
+    Returns
+    -------
+    L : Final wavelength
+    n : Number of iterations performed
+    """
+    gravity = 9.81
+
+    # Initial guess (deep water wave)
+    L = [(gravity * wave_period ** 2) / (2 * np.pi)]
+    n = 1
+
+    while True:
+        L_next = ((gravity * wave_period ** 2) / (2 * np.pi)) * np.tanh((2 * np.pi * water_depth) / L[n - 1])
+        L.append(L_next)
+
+        # Convergence check
+        if abs(L[n-1] / L[n] - 1) < 1e-5:
+            break
+
+        n += 1
+
+        if n > 1000:
+            print("waveLengthIteration did not converge!")
+            break
+
+    print(f"Wave length found to be {L[-1]:.2f} m after {n} iterations.")
+    return L[-1]
