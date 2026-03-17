e127d4355ab8526643bf202577d9527578c3826f
Kasper Blokhus <kasperblokhus@gmail.com>
2026-03-17 12:33:24 +0100

    Add breakwater design script: calculate overtopping, damage, reflection, and transmission parameters

---
+++ Wave calculations/Design of breakwater.py
@@ -0,0 +1,129 @@
+import numpy as np
+from helper_functions import waveLengthIteration
+
+# %% Known values
+g = 9.82
+h = 11.5
+Hs = 6.5
+Hm0 = 6.5
+Hmax = 11.7
+T_10 = 16.3
+Tm = 13.7
+Tp = 16.5
+Nw = 1000
+
+# Structure
+Dn50 = np.array([2, 0.9, 0.4])
+Thickness = np.array([4, 2.7])
+PermeableStructure = 1  # 1 = yes
+Gc = 3 * Dn50[0]
+q_criteria = 5
+slope_angle = np.arctan(1/2) # acot(2) is atan(1/2)
+
+# %% Wave parameters
+L_mDeep = (g * Tm**2) / (2 * np.pi)
+L_10Deep = (g * T_10**2) / (2 * np.pi)
+L_0pDeep = (g * Tp**2) / (2 * np.pi)
+
+k_10 = wave_number(g, T_10, h)
+L_10 = 2 * np.pi / k_10
+
+s_m = Hs / L_mDeep
+s_10 = Hm0 / L_10Deep
+s_0p = Hs / L_0pDeep
+
+xi_m = np.tan(slope_angle) / np.sqrt(s_m)
+xi_10 = np.tan(slope_angle) / np.sqrt(s_10)
+xi_0p = np.tan(slope_angle) / np.sqrt(s_0p)
+
+# %% (1) Overtopping EurOtop
+gamma_f = 0.4
+gamma_beta = 1
+
+if xi_10 > 5:
+    gamma_fmod = min(gamma_f + (xi_10 - 5) * (1 - gamma_f) / 5, 1)
+    if PermeableStructure == 1:
+        gamma_fmod = min(gamma_fmod, 0.6)
+else:
+    gamma_fmod = gamma_f
+
+q = 10000000.0
+R_c = 0.0
+while q - q_criteria > 1e-9:
+    q = (0.09 * np.exp(-(1.5 * R_c / (Hm0 * gamma_fmod * gamma_beta))**1.3)) * np.sqrt(g * Hm0**3) * 1000
+    R_c += 0.00001
+
+R_cEurOtop = R_c
+print(f"R_cEurOtop: {R_cEurOtop:.4f}")
+
+# %% (2) Overtopping Eldrup
+gamma_fS = min(gamma_f + 0.05 * s_10**-0.5 - 0.07 * min(1/np.tan(slope_angle), 3) - 0.09, 1)
+Gcstar = Gc
+gamma_cw = min(1.1 * np.exp(-0.18 * Gcstar / Hm0), 1)
+
+q = 10000000.0
+R_c = 0.0
+while q - q_criteria > 1e-9:
+    q = (0.09 * np.exp(-(1.5 * R_c / (Hm0 * gamma_fS * gamma_beta * gamma_cw))**1.3)) * np.sqrt(g * Hm0**3) * 1000
+    R_c += 0.00001
+
+R_cEldrup = R_c
+print(f"R_cEldrup: {R_cEldrup:.4f}")
+
+# %% (3) P-Estimation
+N = len(Dn50)
+zstar1 = np.zeros(N)
+zstar2 = np.zeros(N)
+
+for i in range(N):
+    if i == 0:
+        zstar1[i] = 0
+        zstar2[i] = Thickness[i] / Dn50[0]
+    elif i == N - 1:
+        zstar1[i] = Thickness[i-1] / Dn50[0] + zstar1[i-1]
+        zstar2[i] = 13
+    else:
+        zstar1[i] = Thickness[i-1] / Dn50[0] + zstar1[i-1]
+        zstar2[i] = Thickness[i] / Dn50[0] + zstar2[i-1]
+
+k_vals = (0.79 - 0.79 * np.exp(-4.1 * Dn50 / Dn50[0])) * ((np.exp(-0.62 * zstar1) - np.exp(-0.62 * zstar2)) / 0.62)
+P = max(0.1, 1.72 * np.sum(k_vals) - 1.58)
+print(f"P: {P:.4f}")
+
+# %% (4) Sd
+gamma_a = 2650
+gamma_w = 1025
+Delta = (gamma_a / gamma_w - 1)
+Dn50_A = Dn50[0]
+
+xi_criteria = (6.2 * P**0.31 * np.sqrt(np.tan(slope_angle)))**(1 / (P + 0.5))
+
+if xi_m < xi_criteria:
+    S = (Hs / (Delta * Dn50_A * 6.2 * P**0.18 * xi_m**-0.5))**(1 / 0.2) * np.sqrt(Nw)
+else:
+    cot_alpha = 1 / np.tan(slope_angle)
+    S = (Hs / (Delta * Dn50_A * 1 * P**-0.13 * xi_m**P * np.sqrt(cot_alpha)))**(1 / 0.2) * np.sqrt(Nw)
+
+print(f"S: {S:.4f}")
+
+# %% (5) Toe Damage
+tt = 2 * Dn50_A
+ht = h - tt
+Bt = 3 * Dn50_A
+k_toe = 2 * np.pi / (g * T_10**2 / (2 * np.pi))
+udelta = np.pi * Hs / T_10 * 1 / np.sinh(k_toe * ht)
+Nod = 0.032 * (tt / Hs) * (Bt / Hs)**0.3 * (Hs / (Delta * Dn50_A))**3 * (udelta / np.sqrt(g * Hs))
+print(f"Nod: {Nod:.4f}")
+
+# %% (6) Reflection
+KR0, KR1 = 0.2, 0.8
+gamma_refl = 3.55
+a_refl = 0.18
+Cr = (KR1 - KR0) * (1 + ((h / (L_10 * np.tan(slope_angle))) / a_refl)**gamma_refl)**-1 + KR0
+print(f"Cr: {Cr:.4f}")
+
+# %% (7) Transmission
+b_trans = -5.42 * s_0p + 0.0323 * Hs / Dn50_A - 0.0017 * (Gc / Dn50_A)**1.84 + 0.51
+Ct = (0.031 * Hs / Dn50_A - 0.24) * R_cEurOtop / Dn50_A + b_trans
+Ct = np.clip(Ct, 0.075, 0.75)
+print(f"Ct: {Ct:.4f}")
