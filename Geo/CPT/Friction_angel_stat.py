f61f42988dff951c7fff286ff7aa71f511735fd5
ludvi <ludvigskj392@gmail.com>
2026-03-24 13:08:27 +0100

    quality of life stuff

--- Geo/CPT/Friction_angel_stat.py
+++ Geo/CPT/Friction_angel_stat.py
@@ -1,11 +1,29 @@
-from CPT_Angle_of_friction_3 import avg_phi_per_layer3
-from CPT_Angle_of_friction_14 import avg_phi_per_layer14
+from CPT_Angle_of_friction_3 import phi_peak_KM3
+from CPT_Angle_of_friction_14 import phi_peak_KM14
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+df = pd.DataFrame()
 
-print(f"Friction angle for CPT3: {avg_phi_per_layer3}")
-print(f"Friction angle for CPT14 {avg_phi_per_layer14}")
+# Now these will work:
+df['phi_peak_KM14'] = phi_peak_KM14
+df['phi_peak_KM3'] = phi_peak_KM3
 
-avg_phi_3 = avg_phi_per_layer3.mean()
-avg_phi_14 = avg_phi_per_layer14.mean()
+# Combine them (Assuming these are strings)
+phi_combined = pd.concat([df['phi_peak_KM14'], df['phi_peak_KM3']], ignore_index=True)
 
-print(f"Friction angle for CPT3: {avg_phi_3:.2f}")
-print(f"Friction angle for CPT14 {avg_phi_14:.2f}")
+# Hvis du vil gemme det i et nyt DataFrame
+df_phi = pd.DataFrame({'phi_peak_total': phi_combined})
+
+phi_mu = np.mean(df_phi)
+phi_std = np.std(df_phi)
+phi_c = stats.norm.ppf(0.05, loc=phi_mu, scale=phi_std)
+phi_d = np.degrees(np.arctan(np.tan(np.radians(phi_c)) / 1.2))
+
+
+
+
+
+print(phi_c)
+print(phi_d)
+
