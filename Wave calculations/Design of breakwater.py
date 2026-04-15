10fc024ac3f3b03b01a04dab45a7ade94daff077
Kasper Blokhus <kasperblokhus@gmail.com>
2026-04-15 12:17:29 +0200

    Enhance breakwater design script: adjust slope, add toe dimensions, refine stability checks, and improve damage output formatting.

--- Wave calculations/Design of breakwater.py
+++ Wave calculations/Design of breakwater.py
@@ -3,6 +3,7 @@ from helper_functions import waveLengthIteration
 
 GREEN = '\033[32m'
 RED = '\033[31m'
+YELLOW = '\033[33m'
 RESET = '\033[0m'
 
 # Known values
@@ -19,8 +20,11 @@ Dn50 = np.array([1.68, 0.64, 0.2]) # Stone size when looking at the armour layer
 PermeableStructure = True  # If the breakwater is permeable to water.
 Gc = 3 * Dn50[0] # Is the width of the crest. [m]
 q_criteria = 0.01 # The amount of water that is allowed to over-top. [m3/s per m]
-slope=1/2 #top is y bottom is x
+slope=1/2.5 #top is y bottom is x
 slope_angle = np.arctan(slope) # The slope of the breakwater. np.arctan(1/2) = is a slope of 1:2.
+
+t_t=2*Dn50[0] # Height of toe
+B_t=3*Dn50[0] # Width of toe
 
 # Thickness of layers
 c_thickness = 1 # 1 because of rock armour type
@@ -113,6 +117,14 @@ else:
     S = (Hs / (Delta * Dn50_A * 1 * P**-0.13 * xi_m**P * np.sqrt(cot_alpha)))**(1 / 0.2) * np.sqrt(Nw)
 
 print(f"S: {S:.4f}")
+if S < 2:
+    print(f"{GREEN}The breakwater sustains initial damage.{RESET}\n")
+elif 4 <= S <= 6:
+    print(f"{GREEN}The breakwater sustains intermediate damage.{RESET}\n")
+elif S >= 8:
+    print(f"{RED}The breakwater leads to failure. The filter layer is exposed.{RESET}\n")
+else:
+    print(f"{YELLOW}The breakwater is transitioning/intermediate state.{RESET}\n")
 
 # (5) Toe Damage
 tt = 2 * Dn50_A
@@ -121,7 +133,7 @@ Bt = 3 * Dn50_A
 k_toe = 2 * np.pi / (g * T_10**2 / (2 * np.pi))
 udelta = np.pi * Hs / T_10 * 1 / np.sinh(k_toe * ht)
 Nod = 0.032 * (tt / Hs) * (Bt / Hs)**0.3 * (Hs / (Delta * Dn50_A))**3 * (udelta / np.sqrt(g * Hs))
-print(f"Nod: {Nod:.4f}")
+print(f"Nod: {Nod:.4f}") # Also done later
 
 # (6) Reflection
 KR0, KR1 = 0.2, 0.8
@@ -134,7 +146,7 @@ print(f"Cr: {Cr:.4f}")
 b_trans = -5.42 * s_m + 0.0323 * Hs / Dn50_A - 0.0017 * (Gc / Dn50_A)**1.84 + 0.51
 Ct = (0.031 * Hs / Dn50_A - 0.24) * R_cEurOtop / Dn50_A + b_trans
 Ct = np.clip(Ct, 0.075, 0.75)
-print(f"Ct: {Ct:.4f}")
+print(f"Ct: {Ct:.4f}\n")
 
 #Scour (slope 1:2 is safe)
 #For a breakwater with a front slope of 1:1.2 the scour protection should cover 0.15L of the seabed and for a slope of 1:1.75 the scour should cover 0.08L.
@@ -165,8 +177,8 @@ dn50_max = Hs / (Delta * 1)
 dn50_min = Hs / (Delta * 4)
 
 print(f"Dn50 must be between {dn50_min:.3f} m and {dn50_max:.3f} m")
-print(f'The armour unit Dn50 is {Dn50[0]} m')
-if 1 < Ns < 4:
+print(f'The armour unit Dn50A is {Dn50[0]} m')
+if dn50_min < Dn50[0] < dn50_max:
     print(f'{GREEN}The stone size fits the requirement{RESET}\n')
 else:
     print(f'{RED}The stone size does not fit the requirement{RESET}\n')
@@ -209,14 +221,21 @@ elif side1 < 0:
 
 #Toe stability
 T_m1=T_10
-t_t=1
 h_t=h-t_t
-B_t=4*Dn50[1]
 
-k=2*np.pi / (9.82/(2*np.pi) * T_m1**2)
-mu_s=np.pi*Hs / T_m1 * 1 / np.sinh(k*h_t)
-N_OD=0.032*(t_t/Hs)*(B_t/Hs)**0.3 * (Hs/(Delta*Dn50[1]))**3 * (mu_s/np.sqrt(9.82*Hs))
+k=2*np.pi / (g/(2*np.pi) * T_m1**2)
+mu_s=(np.pi*Hs) / T_m1 * 1 / np.sinh(k*h_t)
+N_OD=0.032*(t_t/Hs)*(B_t/Hs)**0.3 * (Hs/(Delta*Dn50[0]))**3 * (mu_s/np.sqrt(9.82*Hs))
 print(f"N_OD: {N_OD:.2f}")
+if N_OD < 0.5:
+    print(f"{GREEN}Toe stability leads to initial damage.{RESET}\n")
+elif 2 <= N_OD <= 3:
+    print(f"{GREEN}Toe stability leads to moderate to severe damage. This is often used as a design criterion.{RESET}\n")
+elif N_OD >= 5:
+    print(f"{RED}Toe stability leads to failure.{RESET}\n")
+else:
+    print(f"{YELLOW}Toe stability is transitioning/intermediate state.{RESET}\n")
+
 
 # Stability of the roundhead
 
