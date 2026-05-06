f102ea64721b7395c4102f87c4cce5c701b3f507
as50mg <as50mg@student.aau.dk>
2026-05-06 09:19:13 +0200

    fixed strings

--- Wave calculations/entrance disturbance/after/after 290.py
+++ Wave calculations/entrance disturbance/after/after 290.py
@@ -15,10 +15,10 @@ t=np.arange(0,len(dist[locations[0]]),1)
 
 plt.figure()
 for loc in locations:
-    plt.plot(t,dist[loc],label=f'{loc} disturbance coefficient')
+    plt.plot(t,dist[loc],label=f'{loc} Diffraction coefficient')
 plt.legend(prop={'size': 8})
 plt.xlabel('time [s]')
-plt.ylabel('Disturbance Coefficient [-]')
+plt.ylabel('Diffraction Coefficient [-]')
 plt.savefig(f'raw dist coeff.png',bbox_inches='tight')
 
 
@@ -35,10 +35,10 @@ for loc in locations:
 
 plt.figure()
 for loc in locations:
-    plt.plot(t[idx:len(t)],stable[loc],label=f'{loc}, AVG disturbance coefficient = {np.round(dist_coeff[loc],3)}')
+    plt.plot(t[idx:len(t)],stable[loc],label=f'{loc}, AVG diffraction coefficient = {np.round(dist_coeff[loc],3)}')
 plt.legend(prop={'size': 8})
 plt.ylim([0,2*dist_coeff[locations[0]]])
 plt.xlabel('time [s]')
-plt.ylabel('Disturbance Coefficient [-]')
+plt.ylabel('Diffraction Coefficient [-]')
 plt.savefig(f'stable dist coeff.png',bbox_inches='tight')
 
