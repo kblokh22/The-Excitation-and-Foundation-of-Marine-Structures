5d9d6e35c739bf37657eae915476155139759eed
as50mg <as50mg@student.aau.dk>
2026-05-06 14:17:57 +0200

    fixed strings

---
+++ Wave calculations/Before expansion calculations/1,96m 30min 290deg irregular/analysis.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df=pd.read_excel('distcoeff.xlsx',skiprows=1)
+
+locations=['loc 1','loc 2','loc 3','loc 4','loc 5','loc 6','loc 7','loc 10']
+
+dist={}
+for i in [1,2,3,4,5,6,7,8]:
+    dist[locations[i-1]]=df.iloc[:,i]
+print(dist)
+
+t=np.arange(0,len(dist[locations[0]]),1)
+
+plt.figure()
+for loc in locations:
+    plt.plot(t,dist[loc],label=f'{loc}')
+plt.legend(prop={'size': 8},loc='upper left')
+plt.xlabel('Time [s]')
+plt.ylabel('Diffraction Coefficient [-]')
+plt.savefig(f'raw dist coeff.png')
+
+
+
+idx=int(len(dist[locations[0]])*(23/30))
+print(idx)
+stable={}
+for loc in locations:
+    stable[loc]=dist[loc][idx:]
+
+dist_coeff={}
+for loc in locations:
+    dist_coeff[loc]=np.mean(stable[loc])
+
+plt.figure()
+for loc in locations:
+    plt.plot(t[idx:len(t)],stable[loc],label=f'{loc}')
+plt.legend(prop={'size': 8})
+plt.xlabel('Time [s]')
+plt.ylabel('Diffraction Coefficient [-]')
+plt.savefig(f'stable dist coeff.png')
+
